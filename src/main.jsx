4ef70cfa59463f73a98ea02265fae280eb8c9a79
Eyal Hecht <eyalhe3@gmail.com>
2025-10-31 18:26:06 +0200

    Add loading spinner on mount isntead of loading... text

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import {LoadScript} from "@react-google-maps/api";
 import { ThemeProvider as ShadcnThemeProvider } from "@/components/theme-provider"
+import MapLoadingState from "@/components/MapLoadingState.jsx"
 
 const theme = createTheme();
 
@@ -14,8 +14,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <LoadScript googleMapsApiKey={import.meta.env.VITE_MAPS_API_KEY}>
-                    {/*<CssBaseline />*/}
+                <LoadScript
+                    googleMapsApiKey={import.meta.env.VITE_MAPS_API_KEY}
+                    loadingElement={<MapLoadingState />}
+                >
                     <ShadcnThemeProvider defaultTheme="light" storageKey="echoes-ui-theme">
                     <App />
                     </ShadcnThemeProvider>
