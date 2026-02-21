d3ad40fe5702389e2bc9308d8bc8838d8efd3e97
Eyal Hecht <eyalhe3@gmail.com>
2026-02-21 13:19:44 +0200

    Update Polaroid.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -86,10 +86,10 @@ function Login() {
             />
 
             {/* Scattered polaroids */}
-            <Polaroid rotate="-8deg"  label="Paris, 1963"     tint="#C9A87A" size="sm" className="top-[10%] left-[4%]"    />
-            <Polaroid rotate="6deg"   label="Brooklyn, 1952"  tint="#A89070" size="sm" className="top-[8%]  right-[5%]"   />
-            <Polaroid rotate="-4deg"  label="First car, 1971" tint="#9A8060" size="sm" className="bottom-[12%] left-[3%]" />
-            <Polaroid rotate="9deg"   label="Grandma & Joe"   tint="#B8927A" size="sm" className="bottom-[10%] right-[4%]"/>
+            <Polaroid imageUrl="https://firebasestorage.googleapis.com/v0/b/echoes-prod-2afe6.firebasestorage.app/o/post_media%2FulTS5Y2Y6ab31Z5v0VNZibJnsRV2%2F1762101847784_Scan%202.11.2025%2C%2010.36.jpg?alt=media&token=edb8677e-f7e7-4db8-b27c-5d8983cb0600" rotate="-8deg"  label="Paris, 1963"     tint="#C9A87A" size="md" className="top-[10%] left-[4%]"    />
+            <Polaroid imageUrl="https://firebasestorage.googleapis.com/v0/b/echoes-prod-2afe6.firebasestorage.app/o/post_media%2FulTS5Y2Y6ab31Z5v0VNZibJnsRV2%2F1762103986842_Jena_1960er.jpg?alt=media&token=05bd1ea6-a90e-4219-926a-8d1042d4514b" rotate="6deg"   label="Grandma & Joe"  tint="#A89070" size="md" className="top-[8%]  right-[5%]"   />
+            <Polaroid imageUrl="https://firebasestorage.googleapis.com/v0/b/echoes-prod-2afe6.firebasestorage.app/o/post_media%2FulTS5Y2Y6ab31Z5v0VNZibJnsRV2%2F1762104605109_Goethe_Wohnhaus_Weimar_1966.jpg?alt=media&token=f96fc128-856b-4c94-b72b-5e1a519b581f" rotate="-4deg"  label="First car, 1971" tint="#9A8060" size="md" className="bottom-[12%] left-[3%]" />
+            <Polaroid imageUrl="https://firebasestorage.googleapis.com/v0/b/echoes-prod-2afe6.firebasestorage.app/o/post_media%2FulTS5Y2Y6ab31Z5v0VNZibJnsRV2%2F1762106202384_Ga%CC%88nsema%CC%88nnchenbrunnen_Weimar_1966.jpg?alt=media&token=adee0f45-83b9-43aa-89eb-1c240aef8c51" rotate="9deg"   label="Brooklyn, 1952"   tint="#B8927A" size="md" className="bottom-[10%] right-[4%]"/>
 
             {/* Card */}
             <div className="relative w-full max-w-md z-10">
